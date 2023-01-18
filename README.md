3e7dc8237b2c6c22e69c83a4a6b49f508e8ff669
etapm <etap@ucdavis.edu>
2023-01-18 11:35:32 -0800

    full quiz code

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # js-mini-quiz
+## Description
+The motivation behind this project was to enhance my coding skills by building a fun and interactive Javascript Quiz in a game format. The aim was to make it easy and enjoyable for users to reinforce their understanding of Javascript while competing with others on the same device. By gamifying the studying process, the project addresses the problem of tedious and repetitive studying. Through this project, I have gained valuable experience in making studying fun and interactive. Although there are areas for improvement, I am confident in my ability to continue developing this project in a direction that will benefit both users and the product. The unique aspect of this project is its ability to engage users and encourage them to improve their scores through reinforcing their Javascript knowledge, making it a mutually beneficial experience for all parties.
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+Review the code on the GitHub repository. The deployed site can be accessed at [GitHub Pages](https://etapm.github.io/password-generator-03/) or by visiting the [GitHub Repository](https://github.com/etapm/password-generator-03).
+## Usage
+Upon initiating the quiz by pressing the "start quiz" button, the user will be presented with 10 basic Javascript questions, each with 4 options to choose from, within a 75-second time frame. Incorrect answers will result in a penalty of 10 seconds to the user's score. Upon completion of the quiz or expiration of the time limit, the user will be prompted to enter their initials and save their scores, clear the scores, or return to the home page for another round.
+
+##### Step 1: Click the "Start Quiz" button.
+![Start Quiz page](assets/images/Screenshot-Home.png)
+
+##### Step 2: Click on your answer.
+![Question with four options and a timer on right upper corner](assets/images/Screenshot-TimerHigh.png)
+
+##### Step 3: Click "Save Scores", "Clear Scores", or "Go back".
+![User's score with option to save, clear scores, or link to home page](assets/images/Screenshot-Scores.png)
+
+## Credits
+N/A
+
+## License
+N/A
+
+## Badges
+N/A
+
+## Features
+N/A
+
+## How to Contribute
+N/A
+
+## Tests
+N/A
---
+++ assets/js/script.js
@@ -0,0 +1,221 @@
+// hide the  title
+function hideTitle() {
+    document.getElementById("title").style.display = "none";
+  }
+  
+// questions for the quiz
+const questions = [
+    {
+        question: "How do you refer to an external script called 'xxx.js'?",
+        choices: [
+            "a. script name='xxx.js'",
+            "b. script src='xxx.js'",
+            "c. script href='xxx.js'",
+            "d. script ref='xxx.js'"
+        ],
+        answer: 1
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choices: [
+            "a. function myFunction() {}",
+            "b. function:myFunction() {}",
+            "c. myFunction function() {}",
+            "d. function = myFunction() {}"
+        ],
+        answer: 0
+    },
+    {
+        question: "What is the difference between let and var in JavaScript?",
+        choices: [
+            "a. let is block-scoped, var is function-scoped",
+            "b. let is function-scoped, var is global-scoped",
+            "c. let is hoisted, var is not",
+            "d. let is new, var is old"
+        ],
+        answer: 0
+    },
+    {
+        question: "How do you declare a variable in JavaScript?",
+        choices: [
+            "a. const myVariable;",
+            "b. var myVariable;",
+            "c. let myVariable;",
+            "d. variable myVariable;"
+        ],
+        answer: 1
+    },
+    {
+        question: "What is the difference between == and === in JavaScript?",
+        choices: [
+            "a. == checks for type, === checks for value",
+            "b. == checks for identity, === checks for equality",
+            "c. == checks for value, === checks for type",
+            "d. == checks for equality, === checks for identity"
+        ],
+        answer: 3
+    },
+    {
+        question: "What is the difference between null and undefined in JavaScript?",
+        choices: [
+            "a. null is an object, undefined is a variable",
+            "b. null is intentional no value, undefined is unintentional no value",
+            "c. null is truthy, undefined is falsy",
+            "d. null is a variable, undefined is a function"
+        ],
+        answer: 1
+    },
+    {
+        question: "How do you add an element to an array in JavaScript?",
+        choices: [
+            "a. array.add(element);",
+            "b. array.push(element);",
+            "c. array.append(element);",
+            "d. array[array.length] = element;"
+        ],
+        answer: 1
+    },
+    {
+        question: "How do you create a for loop in JavaScript?",
+        choices: [
+            "a. for i in array",
+            "b. forEach i in array",
+            "c. for (var i = 0; i < array.length; i++)",
+            "d. for i = 0 to array.length"
+        ],
+        answer: 2
+    },
+    {
+        question: "How do you create an array in JavaScript?",
+        choices: [
+            "a. var myArray = {}",
+            "b. var myArray = []",
+            "c. var myArray = new Array()",
+            "d. var myArray = Array.create()"
+        ],
+        answer: 2
+    },
+
+];
+
+let score = 0;
+let currentQuestion = 0;
+let timer;
+let timerInterval = 1000; // 1 second
+let timeLimit = 77; // 77 seconds
+
+//start the quiz on click of start button
+document.getElementById("start-button").addEventListener("click", startQuiz);
+
+// hide the title on start button press
+document.getElementById("start-button").addEventListener("click", hideTitle);
+
+function startQuiz() {
+    hideTitle();
+    document.getElementById("start-button").style.display = "none";
+    document.getElementById("quiz").style.display = "block";
+    startTimer();
+    showQuestion();
+}
+
+//start the timer
+function startTimer() {
+    timer = setInterval(function() {
+        if(timeLimit > 0){
+          timeLimit--;
+          document.getElementById("timer").innerHTML = timeLimit;
+        }
+        if (timeLimit === 0) {
+            clearInterval(timer);
+            endQuiz();
+        }
+    }, timerInterval);
+}
+
+//show the question
+function showQuestion() {
+    let question = questions[currentQuestion].question;
+    let choices = questions[currentQuestion].choices;
+    document.getElementById("question").innerHTML = question;
+    for (let i = 0; i < choices.length; i++) {
+        document.getElementById("choice" + i).innerHTML = choices[i];
+        document.getElementById("choice" + i).addEventListener("click", checkAnswerAndUpdateScore);
+    }
+}
+
+//check the answer and update the score
+function checkAnswerAndUpdateScore(e) {
+    let selected = e.target.innerHTML;
+    let currentAnswer = questions[currentQuestion].answer;
+    let correct = questions[currentQuestion].choices[currentAnswer];
+    if (selected === correct) {
+        score++;
+        document.getElementById("result-message").innerHTML = "Great Job!";
+    } else {
+        timeLimit -= 10;
+        document.getElementById("result-message").innerHTML = "You got this!";
+    }
+    currentQuestion++;
+    if (currentQuestion === questions.length) {
+        endQuiz();
+    } else {
+        showQuestion();
+    }
+}
+
+//end the quiz
+function endQuiz() {
+    clearInterval(timer);
+    document.getElementById("quiz").style.display = "none";
+    document.getElementById("end-screen").style.display = "block";
+    document.getElementById("final-score").innerHTML = score;
+    displayHighScores();
+    document.getElementById("result-message").innerHTML = "";
+    }
+
+ // save score
+function saveScore() {
+    let initials = document.getElementById("initials").value;
+    let scoreObject = {
+        initials: initials,
+        score: score
+    }
+    
+    let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    highScores.push(scoreObject);
+        highScores.sort((a, b) => b.score - a.score);
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+        displayHighScores();
+    }
+    
+    // display high scores
+    function displayHighScores() {
+        let highScoresList = document.getElementById("high-scores");
+        highScoresList.innerHTML = "";
+        let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+        for (let i = 0; i < highScores.length; i++) {
+            let scoreObject = highScores[i];
+            let li = document.createElement("li");
+            li.innerHTML = scoreObject.initials + " - " + scoreObject.score;
+            highScoresList.appendChild(li);
+        }
+    }
+    
+    //clear high scores
+    document.getElementById("clear-scores").addEventListener("click", function() {
+        localStorage.removeItem("highScores");
+        displayHighScores();
+    });
+
+// go back button
+let goBackButton = document.createElement("button");
+goBackButton.innerHTML = "Go Back";
+goBackButton.setAttribute("class", "btn btn-primary");
+goBackButton.addEventListener("click", function(){
+    window.location.href = 'http://127.0.0.1:5500/index.html';
+});
+
+let clearButton = document.getElementById("clear-scores");
+clearButton.parentNode.insertBefore(goBackButton, clearButton.nextSibling);
+
+
